64cf9f638d7444b1854e926f1098861929c80faf
Kae Benton <sonja.k.benton@gmail.com>
2020-08-06 19:02:37 -0400

    completed one stretch goal.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/sokaeb')
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -19,11 +19,33 @@ axios.get('https://api.github.com/users/sokaeb')
     and append the returned markup to the DOM as a child of .cards
 */
 
+// Appending inside the components function 
+// axios.get('https://api.github.com/users/sokaeb')
+// .then(response => profileMaker(response.data))
+// .catch(err => console.log(err))
+
+          // Making own profile card
+axios.get('https://api.github.com/users/sokaeb')
 .then(response => {
-   const profileCard = profileMaker(response);
-   profileCards.appendChild(profileCard);
+  const profileCard = profileMaker(response.data);
+  profileCards.appendChild(profileCard);
 })
 .catch(err => {
+  console.log(err.message); // if there's an error, a message would appear in the console
+});
+
+
+          // Adding personal followers 
+axios.get('https://api.github.com/users/sokaeb/followers') // this is an object
+.then(response => { // object
+  const followersArray = response.data; // this data is an array 
+  followersArray.forEach(follower => {
+    const followerCard = profileMaker(follower);
+    profileCards.appendChild(followerCard); // this is appending into the DOM
+  })
+})
+.catch(err => {
+  console.log(err)
 })
 
 /*
@@ -37,7 +59,25 @@ axios.get('https://api.github.com/users/sokaeb')
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+// Appending inside the components function 
+// followersArray.forEach(follower => {
+//   axios.get(`https://api.github.com/users/${follower}`)
+//   .then(response => profileMaker(response.data))
+//   .catch(err => console.log(err))
+// });
+
+followersArray.forEach(follower =>{
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then(response => {
+    const other = profileMaker(response.data);
+    profileCards.appendChild(other);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+})
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -61,47 +101,52 @@ const followersArray = [];
 const profileCards = document.querySelector('.cards')
 function profileMaker(profileObj) {
   // creating elements needed to markup the DOM with a profile card - instantiating elements
-  const card = document.createElement('div')
-  const image = document.createElement('img')
-  const cardInfo = document.createElement('div')
-  const nameOfUser = document.createElement('h3')
-  const userNamePara = document.createElement('p')
-  const locationPara = document.createElement('p')
-  const profilePara = document.createElement('p')
-  const gitAddress = document.createElement('a')
-  const followersPara = document.createElement('p')
-  const followingPara = document.createElement('p')
-  const bioPara = document.createElement('p')
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const nameOfUser = document.createElement('h3');
+  const userNamePara = document.createElement('p');
+  const locationPara = document.createElement('p');
+  const profilePara = document.createElement('p');
+  const gitAddress = document.createElement('a');
+  const followersPara = document.createElement('p');
+  const followingPara = document.createElement('p');
+  const bioPara = document.createElement('p');
   // setting class names
-  card.classList.add('card')
-  cardInfo.classList.add('card-info')
-  nameOfUser.classList.add('name')
-  userNamePara.classList.add('username')
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  nameOfUser.classList.add('name');
+  userNamePara.classList.add('username');
   // setting attributes and sources
-  image.setAttribute('src', profileObj['avatar_URL'])
-  gitAddress.setAttribute('href', profileObj['html_url'])
+  image.setAttribute('src', profileObj.avatar_url);
+  gitAddress.setAttribute('href', profileObj.html_url);
   // setting text
-  nameOfUser.textContent = profileObj.name
-  userNamePara.textContent = profileObj.login
-  locationPara.textContent = `Location: ${profileObj.location}`
-  profilePara.textContent = `Profile: ${profileObj.html_url}`
-  followersPara.textContent = `Followers: ${profileObj.followers}`
-  followingPara.textContent = `Following: ${profileObj.following}`
-  bioPara.textContent = `Bio: ${profileObj.bio}`
+  nameOfUser.textContent = profileObj.name;
+  userNamePara.textContent = profileObj.login;
+  locationPara.textContent = `Location: ${profileObj.location}`;
+  profilePara.textContent = 'Profile: ';
+  followersPara.textContent = `Followers: ${profileObj.followers}`;
+  followingPara.textContent = `Following: ${profileObj.following}`;
+  bioPara.textContent = `Bio: ${profileObj.bio}`;
+  gitAddress.textContent = profileObj.html_url;
   // creating the hierarchy
-  card.appendChild(image)
-  card.appendChild(cardInfo)
-  cardInfo.appendChild(nameOfUser)
-  cardInfo.appendChild(userNamePara)
-  cardInfo.appendChild(locationPara)
-  cardInfo.appendChild(profilePara)
-  profilePara.appendChild(gitAddress)
-  cardInfo.appendChild(followersPara)
-  cardInfo.appendChild(followingPara)
-  cardInfo.appendChild(bioPara)
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(nameOfUser);
+  cardInfo.appendChild(userNamePara);
+  cardInfo.appendChild(locationPara);
+  cardInfo.appendChild(profilePara);
+  profilePara.appendChild(gitAddress);
+  cardInfo.appendChild(followersPara);
+  cardInfo.appendChild(followingPara);
+  cardInfo.appendChild(bioPara);
   
+  // Appending inside the components function 
+  // const profileCards = document.querySelector('.cards')
+  // profileCards.appendChild(card);
+
   return card
-}
+};
 
 
 /*
@@ -112,3 +157,5 @@ function profileMaker(profileObj) {
     luishrd
     bigknell
 */
+
+// 
